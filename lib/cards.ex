e98f2df7d1ce581672227c55c552bc3aed78da51
Damilare <damilare.durojaye@gmail.com>
2018-04-17 19:38:57 +0100

    Initial Commit

---
+++ lib/cards.ex
@@ -0,0 +1,31 @@
+defmodule Cards do
+  @moduledoc """
+  Documentation for Cards.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Cards.hello
+      :world
+
+  """
+  def hello do
+    "Hi there"
+  end
+
+  def create_deck do
+    ["Ace", "Two", "Three"]
+  end
+
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
+  end
+
+end
